2bc53d9eed5d8978c3d6a872c0365753c25e1919
Andreas Madsen <amwebdk@gmail.com>
2015-03-26 21:55:59 +0100

    [feature] finish initial d3 visualizer

--- visualizer/visualizer.js
+++ visualizer/visualizer.js
@@ -8,33 +8,16 @@
 
   // Settings
   var timeScale = 1e9; // seconds
+  var timeUnit = 'sec';
   var timelineHeight = 20;
 
-  // Setup scale
-  var xScale = d3.scale.linear()
-    .range([10, window.innerWidth - 10])
-    .domain([0, dump.total / timeScale]);
-
-  var xFormat = xScale.tickFormat();
-  var xAxis = d3.svg.axis()
-      .scale(xScale)
-      .orient('top')
-      .tickFormat(function (d) { return (d ? xFormat(d) : '0'); });
-
-  ticks.append('g')
-    .attr('class', 'x axis')
-    .attr('transform', 'translate(0, 24)')
-    .call(xAxis);
-
-  function updateTicks() {
-    xScale.range([10, window.innerWidth - 10]);
-    ticks.select('.x.axis').call(xAxis);
-  }
-
+  //
   // Flatten datastructure
+  //
   function Flatten(data) {
     this.nodes = [];
     this.total = data.total / timeScale;
+    this.version = data.version;
     this.insert(null, data.root);
   }
 
@@ -67,6 +50,38 @@
 
   var flatten = new Flatten(dump);
 
+  //
+  // Set stats
+  //
+    info.select('#stats')
+      .text(`version: ${flatten.version}\ntime: ${flatten.total} ${timeUnit}`);
+
+  //
+  // Setup scale
+  //
+  var xScale = d3.scale.linear()
+    .range([10, window.innerWidth - 10])
+    .domain([0, flatten.total]);
+
+  var xFormat = xScale.tickFormat();
+  var xAxis = d3.svg.axis()
+      .scale(xScale)
+      .orient('top')
+      .tickFormat(function (d) { return (d ? xFormat(d) : '0'); });
+
+  ticks.append('g')
+    .attr('class', 'x axis')
+    .attr('transform', 'translate(0, 24)')
+    .call(xAxis);
+
+  function updateTicks() {
+    xScale.range([10, window.innerWidth - 10]);
+    ticks.select('.x.axis').call(xAxis);
+  }
+
+  //
+  // Draw timeline
+  //
   function drawTimelines() {
     // Update content height
     content.style('height', flatten.totalHeight());
@@ -116,7 +131,37 @@
   }
   drawTimelines();
 
+  //
+  // Show info for timeline
+  //
+  content.on('click', function () {
+    // Calculate the index of the row there was clicked on
+    var rowIndex = Math.floor((
+      d3.event.y + content.node().parentNode.scrollTop -
+      content.node().getBoundingClientRect().top
+    ) / timelineHeight);
+
+    content.selectAll('g .background')
+      .classed('selected', false);
+
+    var row = content.selectAll(`g:nth-child(${rowIndex + 1})`);
+    var node = row.datum();
+
+    // Show only the last 6 callsites
+    var stacktrace = node.stack.slice(-7).map(function (site) {
+      return site.filename + ':' + site.line + ':' + site.column;
+    }).join('\n');
+
+    info.select('#stacktrace')
+      .text('STACKTRACE:\n\n' + stacktrace);
+
+    row.select('.background')
+      .classed('selected', true);
+  });
+
+  //
   // handle resize
+  //
   window.addEventListener('resize', function () {
     updateTicks();
     drawTimelines();
