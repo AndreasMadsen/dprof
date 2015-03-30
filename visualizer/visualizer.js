206a598df80a2a6a47af392f9cd908daa3af5a23
Andreas Madsen <amwebdk@gmail.com>
2015-03-30 21:04:14 +0200

    [refactor] seperate datastructure from layout

--- visualizer/visualizer.js
+++ visualizer/visualizer.js
@@ -1,7 +1,7 @@
 /*eslint-env browser */
 
 var d3 = require('d3');
-var dump = require('./dump.js');
+var flatten = require('./flatten');
 
 // Get elements
 var info = d3.select('#info');
@@ -13,80 +13,6 @@ var content = d3.select('#content');
 var timeScale = 1e9; // seconds
 var timeUnit = 'sec';
 var timelineHeight = 20;
-
-//
-// Flatten datastructure
-//
-function Flatten(data) {
-  this.nodes = [];
-  this.total = data.total / timeScale;
-  this.version = data.version;
-  this.insert(null, data.root);
-}
-
-function unpackFloat(float) {
-  // JSON.stringify converts Infinity to null, which is wired
-  // because Infinity is a part of the double type. So convert
-  // it back, but use a real number, aka the total process time.
-  return float === null ? dump.total : float;
-}
-
-function Node(parent, node, index) {
-  // Meta
-  this.index = index; // related to top position
-  this.id = index; // d3 id, doesn't change
-  this.parent = parent;
-
-  // Info
-  this.name = node.name;
-  this.stack = node.stack;
-
-  // Position
-  this.init = unpackFloat(node.init) / timeScale;
-  this.before = unpackFloat(node.before) / timeScale;
-  this.after = unpackFloat(node.after) / timeScale;
-  this.top = this.index * timelineHeight + timelineHeight / 2;
-}
-
-Flatten.prototype.insert = function (parent, node) {
-  var struct = new Node(parent, node, this.nodes.length);
-  this.nodes.push(struct);
-  node.children.forEach(this.insert.bind(this, struct));
-};
-
-Flatten.prototype.totalHeight = function () {
-  return this.nodes[this.nodes.length - 1].top + timelineHeight / 2;
-};
-
-Flatten.prototype._calcDeltas = function (name, delta) {
-  return this.nodes.map(function (node) {
-    return { 'time': node[name], 'delta': delta };
-  });
-};
-
-Flatten.prototype.overview = function () {
-  // This will give an overview of the concurrency in the process timespan.
-
-  // Create an array of deltas
-  var deltas = this._calcDeltas('init', +1)
-    .concat(this._calcDeltas('after', -1))
-    .sort(function (a, b) {
-      return a.time - b.time;
-    });
-
-  // Now do a communicative sum of the deltas
-  var concurrency = 0;
-  return deltas.map(function (change) {
-    concurrency += change.delta;
-
-    return {
-      'time': change.time,
-      'concurrency': concurrency
-    };
-  });
-};
-
-var flatten = new Flatten(dump);
 
 //
 // Set stats
@@ -190,7 +116,8 @@ drawOverview();
 // This is called when the brush is used or the window is resized
 function drawTimelines() {
   // Update content height
-  content.style('height', flatten.totalHeight());
+  var totalHeight = flatten.nodes[flatten.nodes.length - 1].top + 0.5;
+  content.style('height', totalHeight * timelineHeight);
 
   // Insert data dump
   var bar = content
@@ -206,7 +133,7 @@ function drawTimelines() {
     });
   bar.select('.background')
     .attr('d', function (d) {
-      return `M${xScale(0)} ${d.top}` + // Move to
+      return `M${xScale(0)} ${d.top * timelineHeight}` + // Move to
              `H${xScale(flatten.total)}`; // Horizontal line to
     });
 
@@ -215,15 +142,15 @@ function drawTimelines() {
   bar.select('.init')
     .attr('d', function (d) {
       // Add half after to top1. Add haft befor before top2
-      return `M${xScale(d.init) - 1} ${d.parent.top + 6}` + // Move to
-             `V${d.top + 3}`; // Vertical line to
+      return `M${xScale(d.init) - 1} ${d.parent.top * timelineHeight + 6}` + // Move to
+             `V${d.top * timelineHeight + 3}`; // Vertical line to
     });
 
   barEnter.append('path')
     .attr('class', 'before');
   bar.select('.before')
     .attr('d', function (d) {
-      return `M${xScale(d.init)} ${d.top}` + // Move to
+      return `M${xScale(d.init)} ${d.top * timelineHeight}` + // Move to
              `H${xScale(d.before)}`; // Horizontal line to
     });
 
@@ -231,7 +158,7 @@ function drawTimelines() {
     .attr('class', 'after');
   bar.select('.after')
     .attr('d', function (d) {
-      return `M${xScale(d.before)} ${d.top}` + // Move to
+      return `M${xScale(d.before)} ${d.top * timelineHeight}` + // Move to
              `H${xScale(d.after)}`; // Horizontal line to
     });
 }
