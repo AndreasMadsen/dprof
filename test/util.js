ccc169f3db4ef08dfdb42ce4ed61b59127b37d48
Andreas Madsen <amwebdk@gmail.com>
2016-08-27 21:52:44 +0200

    [test] check timings exists

--- test/util.js
+++ test/util.js
@@ -24,6 +24,10 @@ function simplifyDump(dump) {
   return (function recursive(node) {
     return {
       name: node.name,
+      init: node.init === null ? 'null' : typeof node.init,
+      destroy: node.destroy === null ? 'null' : typeof node.destroy,
+      before: `Array(${node.before.length})`,
+      after: `Array(${node.after.length})`,
       stack: node.stack.map((cite) => cite.filename),
       children: node.children.map(recursive)
     };
